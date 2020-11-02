510b5650dc6112b6c8ffab18054c6999635265b4
Rodrigo Arturo Saffie Kattan <rsaffie@gmail.com>
2020-11-01 23:49:44 -0300

    Add devise users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,37 @@
+# ## Schema Information
+#
+# Table name: `users`
+#
+# ### Columns
+#
+# Name                          | Type               | Attributes
+# ----------------------------- | ------------------ | ---------------------------
+# **`created_at`**              | `datetime`         | `not null`
+# **`current_sign_in_at`**      | `datetime`         |
+# **`current_sign_in_ip`**      | `inet`             |
+# **`email`**                   | `string`           | `default(""), not null, indexed`
+# **`encrypted_password`**      | `string`           | `default(""), not null`
+# **`id`**                      | `bigint(8)`        | `not null, primary key`
+# **`last_name`**               | `string`           |
+# **`last_sign_in_at`**         | `datetime`         |
+# **`last_sign_in_ip`**         | `inet`             |
+# **`name`**                    | `string`           |
+# **`remember_created_at`**     | `datetime`         |
+# **`reset_password_sent_at`**  | `datetime`         |
+# **`reset_password_token`**    | `string`           | `indexed`
+# **`sign_in_count`**           | `integer`          | `default(0), not null`
+# **`updated_at`**              | `datetime`         | `not null`
+#
+# ### Indexes
+#
+# * `index_users_on_email` (_unique_):
+#     * **`email`**
+# * `index_users_on_reset_password_token` (_unique_):
+#     * **`reset_password_token`**
+#
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable,
+         :validatable, :trackable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "repositories#index"
+
   resources :users
   resources :repositories, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
