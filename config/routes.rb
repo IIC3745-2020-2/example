9c61d32144194aefc368b0b82e720ce02e7a2449
Rodrigo Arturo Saffie Kattan <rsaffie@gmail.com>
2020-11-01 23:50:32 -0300

    Add repositories show

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root to: "repositories#index"
 
   resources :users
-  resources :repositories, only: %i[index]
+  resources :repositories, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
